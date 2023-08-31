962311318705e67d6070926c69ff3b6297b775c4
hutl <ericksoares316@gmail.com>
2023-08-31 16:51:57 -0300

    Adiciona os outros membros

--- membros/urls.py
+++ membros/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',IndexView.as_view(), name="index" ),
+    path('',IndexView.as_view(), name="index"),
+    path('membro/<int:pk>',MembroView.as_view(),name='detalhe'),
 ]
--- membros/views.py
+++ membros/views.py
@@ -6,3 +6,7 @@ class IndexView (ListView):
    model = Membro
    template_name= "index.html"
    context_object_name = 'membros'
+
+class MembroView(DetailView):
+    model = Membro
+    template_name = "membro.html"
