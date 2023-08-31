b1cef56228be255f332983df2d7d455cc575a222
Erick <ericksoares316@gmail.com>
2023-08-31 13:45:03 -0300

    first commit

---
+++ membros/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
+from .models import *
+
+class IndexView (ListView):
+   model = Membro
+   template_name= "index.html"
+   context_object_name = 'membros'
